adc55d156854b169875d9de172c79ed4ad5aaf25
redly31 <redlyyyyyyyyyyy@gmail.com>
2024-10-21 20:04:36 +0500

    feat: add error page

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom"
+import "./ui/error.css"
+
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div className="error">
+      <h1 className="error__title">Error!</h1>
+      <h2 className="error__subtitle">Page not found</h2>
+      <Link to="/">To home page</Link>
+    </div>
   )
 }
